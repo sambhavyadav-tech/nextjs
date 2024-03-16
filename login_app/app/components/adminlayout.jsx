cb2174900e4011544d16ff7ed2d8b76dc99c9946
sambhav yadav <sambhavyadav25@gmail.com>
2024-03-16 14:52:21 +0000

    Add admin layout

--- login_app/app/components/adminlayout.jsx
+++ login_app/app/components/adminlayout.jsx
@@ -1,15 +1,30 @@
 // components/adminLayout.js
 
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
+import { useRouter } from 'next/navigation';
 
 const AdminLayout = ({ children }) => {
+    const router = useRouter();
+    const logout = () => {
+        // Implement logout logic here (e.g., remove authentication token, clear session)
+        router.push('/login');
+    };
+
     return (
         <div>
-            <nav className="bg-gray-800 p-4">
-                <div className="container d-inline-flex mx-auto">
-                    <Link href="/admin">Admin Home</Link>
-                    <button onClick={() => logout()} className="text-white ml-4 mx-auto">Logout</button>
+            <nav className="bg-gray-800 text-white py-4 px-4 mb-8 flex justify-between items-center relative">
+                <div className="flex justify-between items-center w-full">
+                    <div>
+                        <Link href="/admin" className="text-xl font-bold"> Home
+                        </Link>
+                        <Link href="/admin" className="text-xl ml-4 font-bold">Applications
+                        </Link>
+                    </div>
+
+                    <button className="fixed top-3 right-4 px-4 py-2 mb-8 bg-red-500 hover:bg-red-600 text-white font-semibold rounded-lg" onClick={() => logout()}>
+                        Logout
+                    </button>
                 </div>
             </nav>
             {children}
